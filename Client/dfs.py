f3b176cb66e0fed641041dfc9561c9a8f1e7dd87
Rustam Gafarov <gafrustam@gmail.com>
2017-08-02 16:47:20 +0300

    Commands on client side parsed

--- Client/dfs.py
+++ Client/dfs.py
@@ -3,27 +3,99 @@ from time import sleep
 import sys
 
 
-def connect_to_naming(naming_ip):
+def pwd(response):
+    # TODO: implement method
+    pass
+
+
+def ls(response):
+    # TODO: implement method
+    pass
+
+
+def cd():
+    # TODO: implement method
+    pass
+
+
+def mkdir():
+    # TODO: implement method
+    pass
+
+
+def touch():
+    # TODO: implement method
+    pass
+
+
+def cp(response):
+    # TODO: implement method
+    pass
+
+
+def rm(response):
+    # TODO: implement method
+    pass
+
+
+def help(args):
+    print "Use $s <namig server ip> <command> [<arguments>]" % args[0]
+    # TODO: describe all commands
+
+
+def execute(args):
+    if len(args) < 3:
+        help(args)
+        return
+
+    command = args[2]
+
+    if command == "help":
+        help(args)
+        return
+
+    no_args_commands = ["pwd", "ls"]
+
+    if command not in no_args_commands:
+        print "Command %s should has arguments"
+        help(args)
+        return
+
+    commands_with_args = []
+    if command not in no_args_commands or command not in commands_with_args:
+        print "There is no command %s"
+        help(args)
+        return
+
+    naming_ip = args[1]
     sock = socket.socket()
-
     sock.connect((naming_ip, 9001))
+    message = ' '.join(args[2:]) # command and arguments
+    sock.send(message)
 
-    print "connected to", naming_ip
-    sys.stdout.flush()
+    if command == "pwd":
+        response = sock.recv(1024)  # 1 KB
+        pwd(response)
+    elif command == "ls":
+        response = sock.recv(1024)  # 1 KB
+        ls(response)
+    elif command == "cd":
+        cd()
+    elif command == "mkdir":
+        mkdir()
+    elif command == "touch":
+        touch()
+    elif command == "cp":
+        response = sock.recv(1048576)  # 1 MB
+        cp(response)
+    elif command == "rm":
+        response = sock.recv(1048576)  # 1 MB
+        rm(response)
 
-    print "Text to send:",
-    text = raw_input()
-    sock.send(text)
-
-    data = sock.recv(1024)  # 1 KB
     sock.close()
-
-    print data
-    sys.stdout.flush()
 
 
 if __name__ == "__main__":
     my_ip = socket.gethostbyname(socket.gethostname())
 
-    naming_ip = sys.argv[1]
-    connect_to_naming(naming_ip)
+    execute(sys.argv)
